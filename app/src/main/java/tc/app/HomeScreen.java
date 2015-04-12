a19be75e70f48e8b049abb4a4b7deba27fd768cf
ssumit <singhsumitbit@gmail.com>
2015-04-12 16:17:42 +0530

    adds app package with a basic activity

---
+++ app/src/main/java/tc/app/HomeScreen.java
@@ -0,0 +1,12 @@
+package tc.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeScreen extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+}
