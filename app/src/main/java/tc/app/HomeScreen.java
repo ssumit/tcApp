286b8a0c24a36f454ee08b9f9276ad92ff20fe81
ssumit <singhsumitbit@gmail.com>
2015-04-15 09:06:15 +0530

    minor ui related change

--- app/src/main/java/tc/app/HomeScreen.java
+++ app/src/main/java/tc/app/HomeScreen.java
@@ -16,7 +16,14 @@ public class HomeScreen extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+        initTitle();
         initRequestButtonListener();
+    }
+
+    private void initTitle() {
+        TextView characterViewTitle = (TextView) findViewById(R.id.home_screen_simple_character_view_title);
+        characterViewTitle.setText(String.format(getString(R.string.home_screen_simple_character_view_title),
+                Integer.toString(BuildConfig.FIRST_QUERY_CHARACTER_POSITION)));
     }
 
     private void initRequestButtonListener() {
@@ -24,18 +31,25 @@ public class HomeScreen extends Activity {
                 .setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        setCharacterRequestView();
+                        setCharacterIteraterView();
+                        setWordCountView();
+                    }
+
+                    private void setCharacterRequestView() {
                         CharacterRequestTextView view = (CharacterRequestTextView) findViewById(R.id.home_screen_simple_character_view);
                         view.fetchFrom(BuildConfig.QUERY_URL);
                         view.showCharacter(BuildConfig.FIRST_QUERY_CHARACTER_POSITION);
-                        TextView characterViewTitle = (TextView) findViewById(R.id.home_screen_simple_character_view_title);
-                        characterViewTitle.setText(String.format(getString(R.string.home_screen_simple_character_view_title), BuildConfig.FIRST_QUERY_CHARACTER_POSITION));
+                    }
 
+                    private void setCharacterIteraterView() {
                         CharacterIteratorTextView characterIteratorTextView =
                                 (CharacterIteratorTextView) findViewById(R.id.home_screen_character_iterator_view);
                         characterIteratorTextView.fetchFrom(BuildConfig.QUERY_URL);
                         characterIteratorTextView.showWithCharFrequency(BuildConfig.CHARACTER_ITERATING_INTERVAL);
-                        characterViewTitle.setText(String.format(getString(R.string.home_screen_simple_character_view_title), BuildConfig.FIRST_QUERY_CHARACTER_POSITION));
+                    }
 
+                    private void setWordCountView() {
                         WordCounterTextView counterTextView = (WordCounterTextView) findViewById(R.id.home_screen_character_count_view);
                         counterTextView.fetchFrom(BuildConfig.QUERY_URL);
                         EditText queryWord = (EditText) findViewById(R.id.home_screen_word_count_query);
